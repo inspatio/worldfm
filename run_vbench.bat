c2224cc7de66e093196b88bbca00edbff52a3177
kschmid <kschmid@nvidia.com>
2026-03-18 21:30:16 +1100

    re

--- run_vbench.bat
+++ run_vbench.bat
@@ -1,17 +1,17 @@
 @echo off
 cd /d "%~dp0"
 
-if not exist venv\Scripts\activate.bat (
+if not exist .venvv\Scripts\activate.bat (
     echo Creating venv...
-    python -m venv venv
+    python -m venv .venvv
     if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 )
-call venv\Scripts\activate.bat
+call .venvv\Scripts\activate.bat
 
 python -m pip install torch==2.7.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128 -q
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-python -m pip install -r requirements.txt --no-build-isolation -q
+python -m pip install -r requirements2.txt --no-build-isolation -q
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 python download_ckpts.py
