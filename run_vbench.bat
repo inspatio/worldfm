f6adcccf8a87e6a0732b3bd09a4bcd1f4640ed31
kschmid <kschmid@nvidia.com>
2026-03-18 22:31:15 +1100

    wheel

--- run_vbench.bat
+++ run_vbench.bat
@@ -14,7 +14,7 @@ if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 python -m pip install torch==2.7.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128 -q
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-python -m pip install -r requirements2.txt --no-build-isolation -q
+python -m pip install -r requirements.txt --no-build-isolation -q
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 python download_ckpts.py
